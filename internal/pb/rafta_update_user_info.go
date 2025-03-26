662c371d386ddc48d4f82aced1f12c32a36db8e4
Benjamin Chausse <benjamin@chausse.xyz>
2025-03-26 01:50:12 -0400

    Rafta/UpdateUserInfo

--- internal/pb/rafta_update_user_info.go
+++ internal/pb/rafta_update_user_info.go
@@ -1,1 +1,25 @@
 package pb
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/ChausseBenjamin/rafta/internal/auth"
+	m "github.com/ChausseBenjamin/rafta/pkg/model"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func (s *raftaServer) UpdateUserInfo(ctx context.Context, data *m.UserData) (*timestamppb.Timestamp, error) {
+	creds, err := auth.GetCreds(ctx, auth.AccessTokenType)
+	if err != nil {
+		return nil, err
+	}
+
+	modified, err := s.updateUser(ctx, creds.UserID, data)
+	if err != nil {
+		return nil, err
+	}
+
+	slog.InfoContext(ctx, "success")
+	return timestamppb.New(modified.AsTime().UTC()), nil
+}
